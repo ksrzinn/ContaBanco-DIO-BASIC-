f799234abdc21293e27b96424ea7878a1c1ea965
Arthur Franca <franca.arth@hotmail.com>
2024-07-26 15:11:19 -0300

    Submetendo projeto de conta utilizando java proposto pelo curso java basico DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    final String AGENCIA = "159";
+    String conta = "";
+    String nome = "";
+    double saldo = 0.0;
+
+
+    public void mostrarConta(){
+        System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ AGENCIA +", conta "+ conta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+
+    public void criarConta(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do titular da conta: ");
+        this.nome = scanner.nextLine();
+
+        System.out.println("Digite o numero da conta desejado: ");
+        this.conta = scanner.nextLine();
+
+        System.out.println("Digite o valor do primeiro deposito: ");
+        this.saldo = scanner.nextDouble();
+
+        this.mostrarConta();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ContaTerminal conta = new ContaTerminal();
+        conta.criarConta();
     }
 }
+
+
+//TO DO: import and use scanner, exibir mensagens
